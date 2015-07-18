e9c9e60d6fd98187e42f29df8698c6f6c03d0c90
Graeme Nelson <graeme.nelson@gmail.com>
2015-07-18 15:02:36 -0700

    filtering out email, password, payment_method_nonce, and access_token from rails controller params

--- config/application.rb
+++ config/application.rb
@@ -19,6 +19,10 @@ module HaaaveApp
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    config.filter_parameters += [:password,
+                                 :email,
+                                 :access_token,
+                                 :payment_method_nonce]
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
