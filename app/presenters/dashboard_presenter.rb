d8428dbd2812632a2cb6cf8f1f03d51fdd9041d5
Graeme Nelson <graeme.nelson@gmail.com>
2015-08-09 13:28:07 -0700

    added dashboard month action template based on state of report

--- app/presenters/dashboard_presenter.rb
+++ app/presenters/dashboard_presenter.rb
@@ -9,17 +9,19 @@ class DashboardPresenter
       @months = []
     end
 
-    def push(report)
-      @months.push(Month.new(report))
+    def push(customer, report)
+      @months.push(Month.new(customer,report))
     end
   end
 
   class Month
 
-    attr_reader :report
+    attr_reader :customer,
+                :report
 
-    def initialize(report)
-      @report = report
+    def initialize(customer, report)
+      @customer = customer
+      @report   = report
     end
 
     def label
@@ -29,6 +31,26 @@ class DashboardPresenter
     def media_count
       report.count
     end
+
+    def template
+      # NOTE: might need to handle current month
+      # currently, all months are completed months
+      return "no_media" unless media_count > 0
+      return "download" if downloadable?
+      return "building" if building?
+      return "order"
+    end
+
+    private
+
+    def downloadable?
+      report.build_pdf_finished_at && report.build_pdf_finished_at <= Time.zone.now
+    end
+
+    def building?
+      report.purchased_at && report.purchased_at <= Time.zone.now
+    end
+
   end
 
   attr_reader :customer
@@ -46,7 +68,7 @@ class DashboardPresenter
   def build_years
     customer.reports.inject([]) do |years, report|
       year = create_or_find_year(years, report)
-      year.push(report)
+      year.push(customer, report)
       years
     end
   end
